9c4e8c2eac0b070ea6ec16fb303f225ee016317a
xqft <estefano.bargas@fing.edu.uy>
2021-08-21 01:21:41 -0300

    Re-escritura general

--- miJuego.pas
+++ miJuego.pas
@@ -1,39 +1,46 @@
+program mijuego;
 
-Program miJuego;
+uses crt;
 
-Uses Crt;
+const
+  X_SIZE = 14;
+  Y_SIZE = 8;
 
-Var horizontalIndex, verticalIndex, xPlayer, yPlayer : Integer;
+var 
+  x_index, y_index, player_x, player_y: integer;
+  keystroke: char;
+  running: boolean;
 
-Var tecla : Char;
-Begin
-  xPlayer := 4;
-  yPlayer := 8;
-  While True Do
-    Begin
-      For horizontalIndex:= 1 To 8 Do
-        Begin
-          For verticalIndex:= 1 To 14 Do
-            Begin
-              If (horizontalIndex = xPlayer) And (verticalIndex = yPlayer) Then
-                write('O')
-              Else
-                write('*');
-              write(' ')
-            End;
-          writeLn('')
-        End;
-      writeLn('Press key');
-      tecla := Readkey;
-      If tecla = 'w' Then
-        xPlayer := xPlayer-1;
-      If tecla = 's' Then
-        xPlayer := xPlayer+1;
-      If tecla = 'd' Then
-        yPlayer := yPlayer+1;
-      If tecla = 'a' Then
-        yPlayer := yPlayer-1;
-      writeLn(tecla);
-      ClrScr;
-    End;
-End.
+begin
+  player_x := X_SIZE div 2;
+  player_y := Y_SIZE div 2;
+  running := true;
+
+  clrscr();
+
+  while running do
+  begin
+    for y_index := 1 to Y_SIZE do
+    begin
+      for x_index := 1 to X_SIZE do
+	if (x_index = player_x) and (y_index = player_y) then
+	  write('o ')
+	else
+	  write('- ');
+      writeln()
+    end;
+
+      writeln('Move with WASD. Exit with ESC.');
+
+      keystroke := readkey();
+      case keystroke of
+	'w': player_y -= 1;
+	's': player_y += 1;
+	'd': player_x += 1;
+	'a': player_x -= 1;
+	#27: running := false; (* #27 = ESC *)
+      end;
+
+      clrscr();
+    end;
+end.
